8d8987ca1990a540c50e2cefdb6d1687c3937bde
Adelaide Yoder <adelaide.yoder@gmail.com>
2018-05-29 10:48:01 -0500

    created the objects

---
+++ main.js
@@ -0,0 +1,39 @@
+const elizabeth = {
+    district: "5th",
+    platformStatements: 
+        {
+            taxes:" ",
+            jobs: " ",
+            infrastructure: " ",
+            healthCare: " ",
+            CrimeAndEnforcement: " ",
+        },
+    donationForm: "url here",
+    calendarOfEvents: "link here",
+    volunteerInfo: [
+        {
+            name:" ",
+            address:" ",
+            email: " ",
+            phoneNumber: " ",
+            availablitiy: " ",
+            activities: " ",
+        },
+        {
+            name:" ",
+            address:" ",
+            email: " ",
+            phoneNumber: " ",
+            availablitiy: " ",
+            activities: " ", 
+        }
+    ],
+    biography: " ",
+    imageGallery: {
+        headShot:" ",
+        pictureOfFamily: " ",
+        pictureOfConstituents: " "
+    },
+    missionStatement:" ",
+    registerToVote: " ",
+}
